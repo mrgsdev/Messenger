306717882f5f6527b4fcd804ae0f1e3fd91ee690
mrgsdev <mrgsdev@gmail.com>
2022-10-29 13:51:19 +0300

    SeachBar

--- Messenger/Messenger/Controllers/NewConversationViewController.swift
+++ Messenger/Messenger/Controllers/NewConversationViewController.swift
@@ -38,22 +38,55 @@ class NewConversationViewController: UIViewController {
     }()
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(noResultsLabel)
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        
         searchBar.delegate = self
         view.backgroundColor = .white
         navigationController?.navigationBar.topItem?.titleView = searchBar
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(dissmissSelf))
         searchBar.becomeFirstResponder()
     }
-     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+        noResultsLabel.frame = CGRect(x: view.width / 4,
+                                      y: (view.height - 200) / 2,
+                                      width: view.width / 2,
+                                      height: 200)
+    }
     @objc private func dissmissSelf(){
         dismiss(animated: true)
     }
 }
+
+extension NewConversationViewController:UITableViewDelegate,UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+         results.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell",for:  indexPath)
+        cell.textLabel?.text = results[indexPath.row]["name"]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: false)
+    }
+    
+    
+}
+
 extension NewConversationViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         guard let text = searchBar.text, !text.replacingOccurrences(of: " ", with: " ").isEmpty else {
             return
         }
+        searchBar.resignFirstResponder()
         results.removeAll()
         spinner.show(in: view)
         self.searchUsers(query: text)
@@ -67,6 +100,8 @@ extension NewConversationViewController: UISearchBarDelegate {
             DatabaseManager.shared.getAllUsers { [weak self] result in
                 switch result{
                 case .success(let usersCollection):
+                    self?.hasFetched = true
+                    
                     self?.users = usersCollection
                     self?.filterUsers(with: query)
                 case .failure(let error):
@@ -80,13 +115,24 @@ extension NewConversationViewController: UISearchBarDelegate {
         guard hasFetched else {
             return
         }
-        
-        var results: [[String:String]] = self.users.filter {
+        self.spinner.dismiss()
+        let results: [[String:String]] = self.users.filter {
             guard let name = $0["name"]?.lowercased() else {
                 return false
             }
             return name.hasPrefix(term.lowercased())
         }
         self.results = results
+        updateUI()
+    }
+    func updateUI(){
+        if results.isEmpty{
+            self.noResultsLabel.isHidden = false
+            self.tableView.isHidden = true
+        }else{
+            self.noResultsLabel.isHidden = true
+            self.tableView.isHidden = false
+            self.tableView.reloadData()
+        }
     }
 }
